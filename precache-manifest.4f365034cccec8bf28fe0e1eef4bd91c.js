807f1b331f46fdf7510200af0314e9aa8c255613
KennyRich <kehinde@chatdesk.com>
2019-10-01 09:31:23 +0100

    Updates

---
+++ precache-manifest.4f365034cccec8bf28fe0e1eef4bd91c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90dded7103fc75840943f7be1ee21fc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "98513f5c7aa40cc2f47c",
+    "url": "/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "245dcfffcfe215264ef7",
+    "url": "/portfolio/static/css/main.744abd52.chunk.css"
+  },
+  {
+    "revision": "98513f5c7aa40cc2f47c",
+    "url": "/portfolio/static/js/2.1c635798.chunk.js"
+  },
+  {
+    "revision": "245dcfffcfe215264ef7",
+    "url": "/portfolio/static/js/main.d1d0a7f6.chunk.js"
+  },
+  {
+    "revision": "c98fad0fa5c777d28f1b",
+    "url": "/portfolio/static/js/runtime-main.36963274.js"
+  }
+]);
